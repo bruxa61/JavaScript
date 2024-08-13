d897b53fb71a2db0b55a5cf3ea2e6e9a041eaa31
Rafaela <rafaelaolbo@gmail.com>
2024-08-13 10:58:18 -0300

    parrumando calculadora de juros compostos e fazendo calculadora de par ou ímpar

--- ParcelascomJuros.js
+++ ParcelascomJuros.js
@@ -14,4 +14,21 @@ console.log("Valor de Cada Parcela:  " + taxaparcelas)
 console.log("''''''''''''''''''''''''''''''''''''''''")
 
 
-for
+// exercicio9_seunome.js
+
+const prompt = require('prompt-sync')();
+
+// Obtém os dados da compra do usuário
+const valorTotalCompra = parseFloat(prompt("Digite o valor total da compra: "));
+const numeroParcelas = parseInt(prompt("Digite o número de parcelas: "));
+const taxaJurosMensal = parseFloat(prompt("Digite a taxa de juros mensal (em porcentagem): ")) / 100;
+
+// Calcula o valor da parcela com juros compostos
+const valorParcela = (valorTotalCompra * taxaJurosMensal) / (1 - Math.pow((1 + taxaJurosMensal), -numeroParcelas));
+
+// Exibe os resultados
+console.log("\n--- Detalhes da Compra ---");
+console.log("Valor Total da Compra: R$", valorTotalCompra.toFixed(2));
+console.log("Número de Parcelas: ", numeroParcelas);
+console.log("Taxa de Juros Mensal: ", (taxaJurosMensal * 100).toFixed(2) + "%");
+console.log("Valor de Cada Parcela: R$", valorParcela.toFixed(2));
--- parouimpar.js
+++ parouimpar.js
@@ -1,3 +1,11 @@
 prompt = require("prompt-sync")();
 
-let pergt = prompt("C")
+// solicita um número ao usuário
+const numero = parseInt(prompt("Digite um número inteiro: "));
+
+// verifica se o número é par ou ímpar
+if (numero % 2 === 0) {
+  console.log(numero + " é um número par.");
+} else {
+  console.log(numero + " é um número ímpar.");
+}
