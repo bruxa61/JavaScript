edf8e3a50db6ac269393fc412f274282cd1c9019
Rafaela <rafaelaolbo@gmail.com>
2024-08-13 17:06:24 -0300

    teste

--- ParcelascomJuros.js
+++ ParcelascomJuros.js
@@ -1,34 +1,13 @@
 prompt = require("prompt-sync")();
 
-let valor = prompt("Valor da Compra: ")
-let parcelas = prompt("Número de Parcela: ")
-let taxa = prompt("Taxa de Juros Mensal: ")
-let total = parseFloat(valor) * parseInt((1 + taxa/100)^parcelas)
-let taxaparcelas = parseInt(total) / parseInt(parcelas)
+let valor = parseFloat(prompt("Valor da Compra: "))
+let parcelas = parseInt(prompt("Número de Parcela: "))
+let taxa = parseFloat(prompt("Taxa de Juros Mensal: ")) / 100
+let totalparcelas = (valor * taxa) / (1 - Math.pow((1 + taxa), -parcelas))
 
 console.log("''''''''''''''''''''''''''''''''''''''''")
-console.log("Valor Total:            " + total)
+console.log("Valor Total:            " + valor.toFixed(2))
 console.log("Número de Parcelas:     " + parcelas)
-console.log("Taxa de Juros:          " + taxa)
-console.log("Valor de Cada Parcela:  " + taxaparcelas)
+console.log("Taxa de Juros:          " + (taxa * 100) + "%")
+console.log("Valor de Cada Parcela:  " + totalparcelas.toFixed(2))
 console.log("''''''''''''''''''''''''''''''''''''''''")
-
-
-// exercicio9_seunome.js
-
-const prompt = require('prompt-sync')();
-
-// Obtém os dados da compra do usuário
-const valorTotalCompra = parseFloat(prompt("Digite o valor total da compra: "));
-const numeroParcelas = parseInt(prompt("Digite o número de parcelas: "));
-const taxaJurosMensal = parseFloat(prompt("Digite a taxa de juros mensal (em porcentagem): ")) / 100;
-
-// Calcula o valor da parcela com juros compostos
-const valorParcela = (valorTotalCompra * taxaJurosMensal) / (1 - Math.pow((1 + taxaJurosMensal), -numeroParcelas));
-
-// Exibe os resultados
-console.log("\n--- Detalhes da Compra ---");
-console.log("Valor Total da Compra: R$", valorTotalCompra.toFixed(2));
-console.log("Número de Parcelas: ", numeroParcelas);
-console.log("Taxa de Juros Mensal: ", (taxaJurosMensal * 100).toFixed(2) + "%");
-console.log("Valor de Cada Parcela: R$", valorParcela.toFixed(2));
--- parouimpar.js
+++ parouimpar.js
@@ -1,9 +1,7 @@
 prompt = require("prompt-sync")();
 
-// solicita um número ao usuário
 const numero = parseInt(prompt("Digite um número inteiro: "));
 
-// verifica se o número é par ou ímpar
 if (numero % 2 === 0) {
   console.log(numero + " é um número par.");
 } else {
