4f5391e32727bcdfc0e8651a3bcc82af42da2521
Rafaela <rafaelaolbo@gmail.com>
2024-08-19 16:54:11 -0300

    aa

--- Navegação/gorjeta.js
+++
@@ -1,9 +0,0 @@
-function gorjetinha() {
-
-    const valor = parseFloat(document.getElementById('ab').value);
-
-    if (Gorjeta == Bom) 
-        {let bompc = 10 * valor / 100 + valor 
-        alert("o valor total é de :" + bompc) 
-    }
-}
